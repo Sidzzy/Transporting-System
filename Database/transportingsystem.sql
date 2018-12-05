524e12e5dc5489f005bf30f07c0c1540b7133a76
Sidzzy <siddh.agrawal008@gmail.com>
2018-12-05 15:57:45 +0530

    Add files via upload

--- Database/transportingsystem.sql
+++ Database/transportingsystem.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 16, 2018 at 07:30 AM
+-- Generation Time: Dec 05, 2018 at 11:25 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.2
 
@@ -32,16 +32,59 @@ CREATE TABLE `driver` (
   `working_under` varchar(100) NOT NULL,
   `trips` int(5) NOT NULL,
   `licence` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `number` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `driver`
 --
 
-INSERT INTO `driver` (`username`, `name`, `working_under`, `trips`, `licence`, `password`) VALUES
-('sidzzy', 'sid singhal', 'sidzzy', 0, 'abcd', '1234'),
-('sidzzzy', 'sid', 'sidzzy', 0, '12', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `driver` (`username`, `name`, `working_under`, `trips`, `licence`, `password`, `number`) VALUES
+('sidzzean', 'Shakaal', 'sidzzy', 0, 'hello world2456', '1234', 9999888822),
+('sidzzy', 'sid singhal', 'sidzzy', 0, 'abcd', '1234', 9938272266),
+('sidzzzy', 'sid', 'sidzzy', 0, '12', '12345', 7978476922);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver_vehicle`
+--
+
+CREATE TABLE `driver_vehicle` (
+  `id` int(10) NOT NULL,
+  `driver_id` varchar(100) NOT NULL,
+  `vehicle_id` varchar(100) NOT NULL,
+  `cost` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `driver_vehicle`
+--
+
+INSERT INTO `driver_vehicle` (`id`, `driver_id`, `vehicle_id`, `cost`) VALUES
+(1, 'sidzzy', 'OD15J1891', 100),
+(2, 'sidzzy', 'OD15J1891', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` int(10) NOT NULL,
+  `passengerusername` varchar(100) NOT NULL,
+  `seats` int(10) NOT NULL,
+  `driver_id` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`id`, `passengerusername`, `seats`, `driver_id`) VALUES
+(2, 'sid1', 1, 'sidzzy');
 
 -- --------------------------------------------------------
 
@@ -111,7 +154,8 @@ CREATE TABLE `vehicle` (
 --
 
 INSERT INTO `vehicle` (`number`, `tspid`, `type`, `capacity`, `source`, `destination`, `timing`, `trips`) VALUES
-('OD15J1890', 'sidzzy', 'Van', 10, 'Burla', 'Sambalpur', '10:00:00', 0);
+('OD15J1890', 'sidzzy', 'Van', 10, 'Burla', 'Sambalpur', '10:00:00', 0),
+('OD15J1891', 'sidzzy', 'Van', 20, 'Burla', 'Sambalpur', '11:00:00', 0);
 
 --
 -- Indexes for dumped tables
@@ -123,6 +167,18 @@ INSERT INTO `vehicle` (`number`, `tspid`, `type`, `capacity`, `source`, `destina
 ALTER TABLE `driver`
   ADD PRIMARY KEY (`username`),
   ADD UNIQUE KEY `licence` (`licence`);
+
+--
+-- Indexes for table `driver_vehicle`
+--
+ALTER TABLE `driver_vehicle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tsp`
@@ -144,6 +200,20 @@ ALTER TABLE `user`
 ALTER TABLE `vehicle`
   ADD PRIMARY KEY (`number`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `driver_vehicle`
+--
+ALTER TABLE `driver_vehicle`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
